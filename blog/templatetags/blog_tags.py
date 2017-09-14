7ab96ac4814c73aba1f4be0e85daa0b3607d119c
809069790 <809069790@qq.com>
2017-09-14 11:39:09 +0800

    更新2017-9-13上午

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from django.db.models.aggregates import Count
-from ..models import Post,Category
+from ..models import Post,Category,Tag
 
 register = template.Library()
 
@@ -15,3 +15,7 @@ def archives():
 @register.simple_tag
 def get_categories():
     return Category.objects.annotate(num_post=Count('post')).filter(num_post__gt=0)
+
+@register.simple_tag
+def get_tags():
+ return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post,Category
+from .models import Post,Category,Tag
 from markdown import markdown
 from django.views.generic import ListView,DetailView
 
 from comment.forms import CommentForm
 from django.http import HttpResponse
 
-# def index(request):
-#     post_list = Post.objects.all()
-#     return render(request,'blog/index.html',context={'post_list':post_list})
-    # return HttpResponse('<h1>hello world</h1>')
 class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
@@ -81,29 +77,6 @@ class IndexView(ListView):
 
         return context
 
-
-
-# def detail(request,pk):
-#     post = get_object_or_404(Post,pk=pk)
-#     post.increase_views()
-#     post.body = markdown(post.body,extensions=[
-#                          'markdown.extensions.extra',
-#                          'markdown.extensions.codehilite',
-#                          'markdown.extensions.toc',])
-#     form = CommentForm()
-#     comment_list = post.comment_set.all()
-#     context = {
-#         'post':post,
-#         'form':form,
-#         'comment_list':comment_list,
-#     }
-#     return render(request,'blog/detail.html',context=context)
-
-# def archives(request,year,month):
-#     post_list = Post.objects.filter(created_time__year=year,
-#                                     created_time__month=month)
-#     return render(request,'blog/index.html',context={'post_list':post_list})
-
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detail.html'
@@ -142,11 +115,12 @@ class ArchivesView(IndexView):
             created_time__month = self.kwargs.get('month')
         )
 
-# def category(request,pk):
-#     cate = get_object_or_404(Category,pk=pk)
-#     post_list = Post.objects.filter(category=cate)
-#     return render(request,'blog/index.html',context={'post_list':post_list})
 class CategoryView(IndexView):
     def get_queryset(self):
         cate = get_object_or_404(Category,pk=self.kwargs.get('pk'))
         return super().get_queryset().filter(category=cate)
+
+class TagView(IndexView):
+    def get_queryset(self):
+        tag = get_object_or_404(Tag,pk=self.kwargs.get('pk'))
+        return super().get_queryset().filter(tags=tag)
